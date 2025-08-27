06c2135130b7db85eb153b90998e067242ecba84
armedvalk <148093690+armedvalk@users.noreply.github.com>
2025-08-27 15:52:44 -0500

    Update FileScanner.py

--- FileScanner.py
+++ FileScanner.py
@@ -1,2 +1,15 @@
 import os
-import pyautogui
+from vt-py import vt
+client = vt.Client(VIRUS_TOTAL_KEY)
+DIRECTORY_SCAN="C:\"
+def DirScan():
+  fileScan=vt.Client.scan_file(DIRECTORY_SCAN)
+  analysis=client.scan_file(f,wait_for_completion=True)
+  if(analysis.waitforcompletion==False):
+    wait(30)
+    print("Exited with code 40")
+    break
+    return(fileScan)
+    
+  
+  
